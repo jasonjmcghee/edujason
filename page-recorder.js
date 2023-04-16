5849abc62e28bb1cbd1f19add3e1d2306b62ba0f
Jason McGhee <mcghee.j@gmail.com>
2023-04-16 04:47:26 -0700

    save everything

---
+++ page-recorder.js
@@ -0,0 +1,232 @@
+const puppeteer = require('puppeteer');
+const { spawn, exec } = require('node:child_process');
+const {writeFileSync, createWriteStream} = require("fs");
+const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
+require('dotenv').config();
+
+function spawnFfmpeg(args) {
+  return new Promise((resolve, reject) => {
+    const ffmpeg = spawn('ffmpeg', args);
+
+    ffmpeg.stdout.on('data', data => {
+      console.log(`stdout: ${data}`);
+    });
+
+    ffmpeg.stderr.on('data', data => {
+      console.error(`stderr: ${data}`);
+    });
+
+    ffmpeg.on('close', code => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`ffmpeg exited with code ${code}`));
+      }
+    });
+  });
+}
+
+async function addBackgroundAudio(videoFile, backgroundAudioFile, volume, outputFile) {
+  try {
+    const args = [
+      '-y',
+      '-i', videoFile,
+      '-i', backgroundAudioFile,
+      '-filter_complex', `[1:a]volume=${volume}[bg];[0:a][bg]amix=inputs=2:duration=first[a]`,
+      '-map', '0:v',
+      '-map', '[a]',
+      '-c:v', 'copy',
+      '-c:a', 'aac',
+      outputFile
+    ];
+    await spawnFfmpeg(args);
+    console.log('Background audio added successfully.');
+  } catch (error) {
+    console.error(`Error while adding background audio: ${error.message}`);
+  }
+}
+
+
+async function concatenateVideos(videos, outputFile) {
+  const videosToConcat = "videos-to-concat.txt";
+  const content = videos.map(video => `file '${video}'`).join('\n');
+  writeFileSync(videosToConcat, content, 'utf8');
+
+  try {
+    const args = ['-y', '-f', 'concat', '-safe', '0', '-i', videosToConcat, '-c', 'copy', outputFile];
+    await spawnFfmpeg(args);
+    console.log('Videos concatenated successfully.');
+  } catch (error) {
+    console.error(`Error while concatenating videos: ${error.message}`);
+  }
+}
+
+const elevenLabsAPI = async (apiKey, text, voice_id, file_name) => {
+  try {
+    const voiceUrl = `https://api.elevenlabs.io/v1/text-to-speech/${voice_id}/stream`;
+    const response = await axios({
+      method: 'post',
+      url: voiceUrl,
+      data: { text },
+      headers: {
+        'Accept': 'audio/mpeg',
+        'xi-api-key': apiKey,
+        'Content-Type': 'application/json',
+      },
+      responseType: 'stream'
+    });
+
+    await new Promise((resolve, reject) => {
+      const writeStream = createWriteStream(file_name);
+      response.data.pipe(writeStream);
+      response.data.on('end', resolve);
+      response.data.on('error', reject);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+async function text_to_mp3(text, filename) {
+  const antoni_voice_id = "ErXwobaYiN019PkySvjV";
+  return await elevenLabsAPI(process.env.ELEVEN_LABS_API, text, antoni_voice_id, filename);
+}
+
+function text_to_mp3_fallback(text, filename) {
+  return new Promise((resolve, reject) => {
+    exec(`say "${text}" -o ${filename}.aiff && sox ${filename}.aiff ${filename}.mp3 && rm ${filename}.aiff`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error while executing ffmpeg command: ${error.message}`);
+        reject(error);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+      }
+      console.log(`stdout: ${stdout}`);
+      resolve();
+    })
+  });
+}
+
+async function getAudioDuration(audioFile) {
+  return new Promise((resolve, reject) => {
+    const ffprobe = spawn('ffprobe', ['-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', audioFile]);
+
+    let duration = '';
+
+    ffprobe.stdout.on('data', data => {
+      duration += data;
+    });
+
+    ffprobe.stderr.on('data', data => {
+      console.error(`stderr: ${data}`);
+    });
+
+    ffprobe.on('close', code => {
+      if (code === 0) {
+        resolve(parseFloat(duration.trim()));
+      } else {
+        reject(new Error(`ffprobe exited with code ${code}`));
+      }
+    });
+  });
+}
+
+async function generateVideoWithAudioAndImage(audioFile, imageFile, outputFile) {
+  return new Promise(async (resolve, reject) => {
+    // Construct the ffmpeg command
+    // const ffmpegCommand = `ffmpeg -loop 1 -i "${imageFile}" -i "${audioFile}" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${outputFile}"`;
+    const audioDuration = await getAudioDuration(audioFile);
+    const ffmpeg = spawn('ffmpeg', [
+      '-y',
+      '-loop', '1',
+      '-i', `${imageFile}`,
+      '-i', `${audioFile}`,
+      '-c:v', 'libx264',
+      '-tune', 'stillimage',
+      '-t', audioDuration.toString(),
+      '-c:a', 'aac',
+      '-b:a', '192k',
+      '-pix_fmt', 'yuv420p',
+      '-shortest',
+      // '-preset', 'ultrafast',
+      // '-crf', '18',
+      outputFile,
+    ], {stdio: ['pipe', 'inherit', 'inherit']});
+
+    ffmpeg.on('close', code => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`ffmpeg exited with code ${code}`));
+      }
+    });
+  });
+}
+
+const makeVideo = (url) => {
+  return new Promise(async (resolve, reject) => {
+    const width = 1920;
+    const height = 1080;
+
+    const browser = await puppeteer.launch({headless: true, defaultViewport: {width, height}, args: [`--window-size=${width},${height + 120}`]});
+    const page = (await browser.pages())[0];
+
+    const data = { images: [], videos: [], audio: [], scripts: [] };
+
+    page.on('console', (message) => {
+      if (message.type() === 'log') {
+        console.log(`Console log: ${message.text()}`);
+      } else if (message.type() === 'error') {
+        console.error(`Console error: ${message.text()}`);
+      }
+    });
+
+    page.exposeFunction("slideschanged", async (script) => {
+      const path = `image-${data.images.length}.png`;
+      data.images.push(path);
+      data.scripts.push(script);
+      await page.screenshot({type: 'png', path: path});
+      // ffmpeg.stdin.write(screenshot);
+    })
+
+    page.exposeFunction('slidesdone', async () => {
+      await browser.close();
+
+      // Do this sequentially because eleven labs didn't like it.
+      // await Promise.all(
+        for (const image of data.images) {
+          const i = data.images.indexOf(image);
+          const audio_filename = `audio-${i}.mp3`;
+          data.audio.push(audio_filename);
+          await text_to_mp3(data.scripts[i], audio_filename);
+        }
+      // );
+
+      await Promise.all(
+        data.images.map((image, i) => {
+          const audio_filename = `audio-${i}.mp3`;
+          const video_filename = `video-${data.videos.length}.mp4`;
+          data.videos.push(video_filename);
+          return generateVideoWithAudioAndImage(audio_filename, image, video_filename);
+        })
+      );
+
+      const concatenatedVideo = 'concatenated_video.mp4';
+      await concatenateVideos(data.videos, concatenatedVideo);
+
+      const backgroundAudioFile = 'lakeside-path.mp3';
+      const volume = '0.3'; // Adjust the background audio volume (0.1 = 10% of original volume)
+      const outputWithBackgroundAudio = 'final-video.mp4';
+
+      await addBackgroundAudio(concatenatedVideo, backgroundAudioFile, volume, outputWithBackgroundAudio);
+      resolve();
+    });
+
+    await page.goto(url, { waitUntil: 'load' });
+  });
+};
+
+module.exports = { makeVideo }
---
+++ server.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const {readFileSync, statSync, createReadStream} = require("fs");
+const {makeVideo} = require("./page-recorder");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(cors({
+  origin: "https://chat.openai.com"
+}));
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+const slideContentPrompt = (subject) => `You are helping build a short educational video which provides "${subject}" which is less than 3 minutes long. POST to /video-script-prompt with JSON of 5-10 slides in markdown format with a title, and optionally code or supporting bullet points.`;
+
+const scriptPrompt = `
+POST to /make-video with the video script, split into an array of script-portions for each slide. Use metaphors, casual speech, and be friendly. Remember the audience will have the slides from before.
+`;
+
+let slideContent = [];
+
+app.get('/sample', (req, res) => {
+  const sample = slideContent.join("\n\n---\n\n");
+  const page = readFileSync('public/index.html', 'utf-8').replace('{replace_me}', sample);
+  console.log(page);
+  res.send(page);
+});
+
+/** This must be called first to generate the prompt necessary for creating initial slides for learning. */
+app.post("/slide-content-prompt", (req, res) => {
+  const jsonData = req.body;
+  res.send({
+    "prompt": slideContentPrompt(jsonData["subject"])
+  })
+});
+
+/** Call this next to ensure the content is stored and the video script prompt is retrieved. */
+app.post("/video-script-prompt", (req, res) => {
+  const jsonData = req.body;
+  slideContent = jsonData["slides"];
+  res.send({"prompt": scriptPrompt})
+});
+
+/** Finally, call this to create the video for our audience. */
+app.post('/make-video', async (req, res) => {
+  const jsonData = req.body;
+  jsonData["slides"].forEach((s, i) => {
+    slideContent[i] += `\nNote:\n\n${s}`;
+  });
+  await makeVideo("http://localhost:3000/sample");
+  res.send({"link": "http://localhost:3000/watch"});
+});
+
+app.use(express.static('public'));
+
+app.get('/watch', (req, res) => {
+  res.send(`<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+    body { margin: 0; background-color: #191919 }; 
+    </style>
+    <title>Video Player</title>
+  </head>
+  <body>
+    <video controls style="width: 100%; height: auto">
+      <source src="http://localhost:3000/video" type="video/mp4">
+    </video>
+  </body>
+</html>`);
+});
+
+
+app.get('/video', (req, res) => {
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send('Range header not found');
+    return;
+  }
+
+  const videoPath = 'final-video.mp4';
+  const stats = statSync(videoPath);
+  const videoSize = stats.size;
+
+  const parts = range.replace(/bytes=/, '').split('-');
+  const start = parseInt(parts[0], 10);
+  const end = parts[1] ? parseInt(parts[1], 10) : videoSize - 1;
+
+  const chunkSize = (end - start) + 1;
+  const fileStream = createReadStream(videoPath, { start, end });
+
+  res.writeHead(206, {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': chunkSize,
+    'Content-Type': 'video/mp4',
+  });
+
+  fileStream.pipe(res);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
